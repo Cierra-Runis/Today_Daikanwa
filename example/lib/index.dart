e04245dca2ded988fe14f414eb37465883437313
Cierra_Runis <2864283875@qq.com>
2023-07-27 21:49:54 +0800

    📖删去 dart 样例

--- example/lib/index.dart
+++
@@ -1,9 +0,0 @@
-export 'dart:convert';
-
-export 'package:flutter/material.dart'
-    hide RefreshIndicator, RefreshIndicatorState;
-
-export 'model/index.dart';
-
-export 'package:dio/dio.dart';
-export 'package:easy_refresh/easy_refresh.dart';
